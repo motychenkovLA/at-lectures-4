bee937d54a696579b94aff0da54d1bb5787936ee
vtsymbulova <vtsymbulova@t1-consulting.ru>
2021-07-21 17:43:24 +0300

    исправлены замечания по ДЗ2

--- src/tracker/Main.java
+++ src/tracker/Main.java
@@ -6,58 +6,56 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean keepRunning = true;
-        String[] resumeOfDef = new String[10];
-        String[] criticalityOfDef = new String[10];
-        int[] daysToFix = new int[10];
+        int defectCounter = 0;
+        final int DEFECT_LIMIT = 10;
+        String[] defectResume = new String[DEFECT_LIMIT];
+        String[] defectCriticality = new String[DEFECT_LIMIT];
+        int[] daysToFix = new int[DEFECT_LIMIT];
         while (keepRunning) {
             System.out.println("Главное меню:\n1.Добавить новый дефект (Введите \"add\"),\n2.Вывести список дефектов (Введите \"list\"),\n3.Выйти из программы (Введите \"quit\")\n");
             System.out.println("Введите команду:");
             String input = scanner.nextLine();
-            keepRunning = !"quit".equals(input);
-            {
-                switch (input) {
-                    case "add":
-                        if (resumeOfDef[resumeOfDef.length - 1] != null) {
-                            System.out.println("Превышено количество дефектов. Система может хранить только 10 дефектов\n");
-                        } else {
-                            for (int i = 0; i < resumeOfDef.length; i++) {
-                                if (resumeOfDef[i] != null) continue;
-                                System.out.println();
-                                System.out.println("Введите резюме дефекта:");
-                                resumeOfDef[i] = scanner.nextLine();
-                                System.out.println("Введите критичность дефекта (очень высокий, высокий, средний, низкий, очень низкий):");
-                                criticalityOfDef[i] = scanner.nextLine();
-                                System.out.println("Введите ожидаемое количество дней на исправление дефекта:");
-                                daysToFix[i] = scanner.nextInt();
-                                scanner.nextLine();
-                                break;
-                            }
-                        }
+            switch (input) {
+                case "add":
+                    if (defectCounter == DEFECT_LIMIT) {
+                        System.out.println("\nПревышено количество дефектов. Система может хранить только 10 дефектов\n");
+                    } else {
+                        System.out.println();
+                        System.out.println("Введите резюме дефекта:");
+                        defectResume[defectCounter] = scanner.nextLine();
+                        System.out.println("Введите критичность дефекта (очень высокий, высокий, средний, низкий, очень низкий):");
+                        defectCriticality[defectCounter] = scanner.nextLine();
+                        System.out.println("Введите ожидаемое количество дней на исправление дефекта:");
+                        daysToFix[defectCounter] = scanner.nextInt();
+                        scanner.nextLine();
+                        ++defectCounter;
+                        System.out.println();
                         break;
-                    case "list":
-                        if (resumeOfDef[0] == null) {
-                            System.out.println("В систему еще не добавлено ни одного дефекта\n");
-                        } else {
+                    }
+                    break;
+                case "list":
+                    if (defectCounter == 0) {
+                        System.out.println("\nВ систему еще не добавлено ни одного дефекта\n");
+                    } else {
+                        System.out.println();
+                        System.out.println("Список дефектов:");
+                        for (int i = 0; i < defectCounter; i++) {
                             System.out.println();
-                            System.out.println("Список дефектов:");
-                            for (int i = 0; i < resumeOfDef.length; i++) {
-                                if (resumeOfDef[i] == null) break;
-                                System.out.println();
-                                System.out.println("Номер: " + i);
-                                System.out.println("Резюме: " + resumeOfDef[i]);
-                                System.out.println("Серьезность: " + criticalityOfDef[i]);
-                                System.out.println("Количество дней на исправление: " + daysToFix[i]);
-                                System.out.println();
-                            }
+                            System.out.println("Номер: " + i);
+                            System.out.println("Резюме: " + defectResume[i]);
+                            System.out.println("Серьезность: " + defectCriticality[i]);
+                            System.out.println("Количество дней на исправление: " + daysToFix[i]);
+                            System.out.println();
                         }
-                        break;
-                    case "quit":
-                        System.out.println("Выход из программы");
-                        break;
-                    default:
-                        System.out.println("Команда не распознана\n");
-                        break;
-                }
+                    }
+                    break;
+                case "quit":
+                    System.out.println("Выход из программы");
+                    keepRunning = false;
+                    break;
+                default:
+                    System.out.println("Команда не распознана\n");
+                    break;
             }
         }
     }
