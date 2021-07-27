d737e9b0e06f04a372891dd21875d56d03dd52f9
Alex1329 <akurganskiy.tsk@gmail.com>
2021-07-27 21:00:39 +0300

    дз 1 - Правки по замечаниям от Артёма

--- src/dz/BugTracking.java
+++ src/dz/BugTracking.java
@@ -5,51 +5,24 @@ import java.util.Scanner;
 public class BugTracking {
     public static void main(String[] args) {
 
+        final int DAYS_IN_WEEK = 5;
+
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Введите описание дефекта:");
-        Scanner str = new Scanner(System.in);
-        String bug_report;
-        bug_report = str.nextLine();
+        String bugReport = scanner.nextLine();
 
         System.out.println("\nВведите критичность дефекта:\nBlocker\nCritical\nMajor\nTrivial");
-        Scanner crit = new Scanner(System.in);
-        String critical;
-        critical = crit.nextLine();
-
-        /*
-        do {
-            System.out.println("Message");
-            critical = critical.nextLine();
-        } while (critical != /////);
-
-        ///
-        if (critical == "Blocker" || critical == "Critical" || critical == "Major" || critical == "Trivial") {
-            System.out.println("Условие работает!!!");
-        } else {
-            System.out.println("Введите критичность дефекта:\nBlocker\nCritical\nMajor\nTrivial");
-        }
-        critical = str.nextLine();
-*/
-/*
-        do {
-            //System.out.println("Пожалуйста, введите значение из списка:\nBlocker\nCritical\nMajor\nTrivial");
-            critical = str.nextLine();
-        } while (critical == "Blocker");
-        while (critical == "Critical");
-        while (critical == "Major");
-        while (critical == "Trivial");
- */
-
-
-        /////////////////
+        String critical = scanner.nextLine();
 
         System.out.println("\nВведите ожидаемое количество дней на исправление дефекта:");
-        Scanner scanner = new Scanner(System.in);
         int termdays = scanner.nextInt();
 
+        boolean isToManyWeek = termdays > DAYS_IN_WEEK;
 
-        System.out.print(bug_report + " | ");
+        System.out.print(bugReport + " | ");
         System.out.print(critical + " | ");
         System.out.print(termdays + " | ");
-        System.out.print("займет больше рабочей недели " + "("+(termdays > 5)+")");
+        System.out.print("займет больше рабочей недели " + "("+(isToManyWeek)+")");
     }
 }
