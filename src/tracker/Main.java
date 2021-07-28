4aab4ff1580f5f67264e52c462774e677117535b
akokaev <alexander.kokaev@gmail.com>
2021-07-28 18:01:18 +0300

    Исправление по DZ1

--- src/tracker/Main.java
+++ src/tracker/Main.java
@@ -4,19 +4,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Резюме дефекта");
+        final int DAYS_IN_WEEK = 7;
         Scanner scanner = new Scanner(System.in);
-        String NameDef = scanner.nextLine();
-        System.out.println("Ввести критичность дефекта из предложенных вариантов: Trivial, Minor, Major, Critical, Blocker");
-        Scanner scanner1 = new Scanner(System.in);
-        String CriticalDef = scanner.nextLine();
-        final int DaysWeek = 7;
-        System.out.println("Ввести кол-во дней на исправление дефекта");
-        Scanner scanner2 = new Scanner(System.in);
-        int DaysEdit = scanner.nextInt();
-        boolean BigWeek = DaysEdit >= DaysWeek;
 
-        System.out.println("Резюме дефекта: " + NameDef + "|Критичность: " + CriticalDef + "|Кол-во дней: " + DaysEdit + "|Займёт больше рабочей недели: " + BigWeek);
+        System.out.println("Резюме дефекта");
+        String errorSummary = scanner.nextLine();
+
+        System.out.println("Ввести критичность дефекта из предложенных вариантов: Trivial, Minor, Major, Critical, Blocker");
+        String errorCriticality = scanner.nextLine();
+
+        System.out.println("Ввести кол-во дней на исправление дефекта");
+        int fixDays = scanner.nextInt();
+
+        boolean isMoreThanAWeek = fixDays >= DAYS_IN_WEEK;
+
+        System.out.println("Резюме дефекта: " + errorSummary + "|Критичность: " + errorCriticality + "|Кол-во дней: " + fixDays + "|Займёт больше рабочей недели: " + isMoreThanAWeek);
 
 
     }
